7ae8630306f65522e388a23e3d806396480eb158
FrankLin23 <linzhangsheng23@163.com>
2022-12-03 23:42:07 +0800

    init project

---
+++ android/app/src/main/kotlin/com/bobby/force_music_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bobby.force_music_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/richcapital/force_music_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.richcapital.force_music_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
